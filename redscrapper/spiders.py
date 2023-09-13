5c166f7379b72f291ae59d736da643c6fd0157df
Jiajun Xu <131632195+JiajunXu2@users.noreply.github.com>
2023-09-12 21:09:33 -0400

    Update spiders.py

--- redscrapper/spiders.py
+++ redscrapper/spiders.py
@@ -1,1 +1,8 @@
+import scrapy
+class RedspiderSpider(scrapy.Spider):
+    name = "redspider"
+    allowed_domains = ["reddit.com"]
+    start_urls = ["https://reddit.com"]
 
+    def parse(self, response):
+        pass
